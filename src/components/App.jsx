57fd38af752c9b9042c2efb6b4b9a4ec38f32d96
Annu Poddar <annupoddar444@gmail.com>
2025-05-24 12:14:24 +0530

    created userSlice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./Body";
-import './App.css';
+import '../App.css';
 import Login from "./Login";
-import Profile from "./Profile";
+import Profile from "../Profile";
+import { Provider } from "react-redux";
+import appStore from "../utils/appStore";
+import Feed from "./Feed";
 
 function App() {
   return (
+    <Provider store={appStore}>
     <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<Body />}>
+          <Route path="/" element={<Feed/>}/>
           <Route path="login" element={<Login/>} />
           <Route path="profile" element={<Profile/>} />
-          <Route path="*" element={<h1>hiiiiii</h1>} />
+          <Route path="*" element={<h1>Page not Found</h1>} />
         </Route>
       </Routes>
     </BrowserRouter>
+  </Provider>
   );
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,24 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BACKEND_API } from "../utils/constants";
 
 const Login = () => {
     const [emailId, setEmailId] = useState("annu@gmail.com");
     const [password, setPassword] = useState("Annu@123");
 
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
     const handleLogin = async() => {
         try {
-            const res = await axios.post("http://localhost:7777/login", {
+            const res = await axios.post(BACKEND_API + "login", {
                 emailId,
                 password
             }, {withCredentials: true})
+            dispatch(addUser(res.data))
+            navigate('/');
         } catch (error) {
             console.error(error)
         }
